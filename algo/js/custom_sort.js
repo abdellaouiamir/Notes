6921c5ae9bb4c881274c5abc54eafa15c9d395eb
abdellaouiamir <abdellaouiamir27@gmail.com>
2025-02-11 19:50:57 +0100

    commit to be squashed later

---
+++ algo/js/custom_sort.js
@@ -0,0 +1,32 @@
+// quick sort
+function quick_sort() {
+}
+
+// merge sort
+function merge_sort() {
+}
+
+// bubble sort
+function bubble_sort() {
+}
+
+// insert sort
+function insert_sort() {
+}
+
+// selection sort
+function selection_sort() {
+}
+
+// test
+let list = [23, 87, 15, 92, 44, 78, 36, 11, 55, 90, 62, 5, 31, 74, 98, 20, 66, 47, 83, 29]
+console.log("quick_sort")
+console.log(quick_sort())
+console.log("merge_sort")
+console.log(merge_sort())
+console.log("bubblesort")
+console.log(bubble_sort())
+console.log("insert_sort")
+console.log(insert_sort())
+console.log("selection_sort")
+console.log(selection_sort())

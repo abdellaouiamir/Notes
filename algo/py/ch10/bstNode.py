ed6d6ad22d527cf7df7de40e88aec474b6067a16
abdellaouiamir <abdellaouiamir27@gmail.com>
2025-07-18 10:22:57 +0100

    delete node from bstree

--- algo/py/ch10/bstNode.py
+++ algo/py/ch10/bstNode.py
@@ -24,17 +24,38 @@ class BSTNode:
             return
         if self.val == val:
             return
-        if val < self.val and self.left is None:
+        if val < self.val:
             if self.left is None:
                 self.left = BSTNode(val)
             else:
                 self.left.insert(val)
-            return
-        if val > self.val:
+        else:
             if self.right is None:
                 self.right = BSTNode(val)
             else:
                 self.right.insert(val)
+
+    def delete(self, val):
+        if self.val is None:
+            return None
+        if val < self.val:
+            if self.left is not None:
+                self.left = self.left.delete(val)
+        elif val > self.val:
+            if self.right is not None:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            else:
+                min_node = self.right
+                while min_node.left is not None:
+                    min_node = min_node.left
+                self.val = min_node.val
+                self.right = self.right.delete(self.val)
+        return self
 
     def get_max(self):
         if self.right is None:
@@ -46,6 +67,9 @@ class BSTNode:
             return self.val
         return self.left.get_min()
 
+    def __repr__(self):
+        return f"BSTNode(val={self.val}, left={self.left is not None}, right={self.right is not None})"
+
 
 if __name__ == "__main__":
     bst = BSTNode()
@@ -55,5 +79,7 @@ if __name__ == "__main__":
     bst.insert(8)
     bst.insert(1)
     bst.insert(233)
-    print(bst.get_max())
-    print(bst.get_min())
+    print(bst.get_max())  # Should print 233
+    print(bst.get_min())  # Should print 1
+    bst = bst.delete(233)  # Delete returns the modified tree
+    print(bst.get_max())  # Should print 10
